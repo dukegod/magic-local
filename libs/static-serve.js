5fafe6ff84dd5c080b48cb7f912fd5083e95e7a5
dukegod <dukehui1991@gmail.com>
2018-10-17 22:59:44 +0800

    update

---
+++ libs/static-serve.js
@@ -0,0 +1,17 @@
+/**
+ * static serve
+ */
+const http = require('http')
+const path = require('path')
+
+class StaticServe{
+  constructor(props) {
+    super(props)
+    this.port = prot;
+    this.root = root;
+  }
+  serve(){
+    http.createServer()
+  }
+
+}
